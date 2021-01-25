0ae7f449f82f2c880ebc4fe9c5641a3f18158aee
Kenneth Paul Tengco <kenneth.tengco@betica.com>
2021-01-25 19:05:41 +0800

    add header module

---
+++ src/translations.js
@@ -0,0 +1,6 @@
+const TRANSLATIONS = {
+    'common.slogan': 'FIND THE RIGHT LAWYER NOW',
+    'common.keyFactor.noFee': 'No fee to present your case',
+    'common.keyFactor.chooseInYourArea': 'Choose from lawyers in your area',
+    'common.keyFactor.confidentialService': 'A 100% confidential service'
+};
